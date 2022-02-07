4b6cc4424221be77e0a5088900be18f62a9e7cb4
DariuszMech <97852400+DariuszMech@users.noreply.github.com>
2022-02-07 20:44:32 +0100

    Delete Dokumentacja HTML directory

--- Dokumentacja HTML/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.dariuszmech"},{"l":"com.dariuszmech.desktop"}];updateSearchResults();
--- Dokumentacja HTML/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.dariuszmech.desktop","l":"DesktopLauncher"},{"p":"com.dariuszmech","l":"EagleEyeCG"}];updateSearchResults();
